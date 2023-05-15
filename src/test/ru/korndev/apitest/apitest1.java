a4dc0e27324de020a8cb014b6906d6da82547491
gitkornienkov <122346047+gitkornienkov@users.noreply.github.com>
2023-05-15 12:13:31 +0300

    Update apitest1.java

--- src/test/ru/korndev/apitest/apitest1.java
+++ src/test/ru/korndev/apitest/apitest1.java
@@ -1,1 +1,13 @@
+package ru.korndev.apitest;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 
+class ApiTest1 {
+  @Test
+// код теста
+      .then()
+          .statusCode(200)
+          // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
+}
